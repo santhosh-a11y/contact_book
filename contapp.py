04e63cc3939e4d67fa8822fe6a7dd8e08eef3410
santhosh <santhosh7kumar7u77@gmail.com>
2025-05-27 07:12:52 +0530

    Add files via upload
    the date's 27-05-2025, 
    project update can write, store and view people's name and their contacts.

---
+++ contapp.py
@@ -0,0 +1,39 @@
+import json
+
+operation = (input("enter operation: "))
+
+# a function to get inputs from the user
+def enterr():
+    
+    try:
+        with open("contacts.txt", "r") as i:
+            contacts_dict = json.load(i)
+    
+    except FileNotFoundError:
+        contacts_dict = {}
+
+    while True:
+        a = input("name: ")
+        if a == "x":
+            break
+        b = int(input("contacts: "))
+
+        contacts_dict[a] = b
+    
+    with open("contacts.txt", "w") as i:
+        json.dump(contacts_dict, i)
+
+
+# a function to view the stored values
+def seee():
+    with open("contacts.txt", "r") as i:
+        content = json.load(i)
+        print(content)
+
+
+# calling the enter function
+if operation == "write":
+    enterr()
+
+elif operation == "see":
+    seee()
